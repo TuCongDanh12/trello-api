f768bf476cb608e2bf3f5552cac45c5e54590987
TuCongDanh12 <tucondanh9a1@gmail.com>
2025-02-07 00:39:34 +0700

    connect DB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,36 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from './environment.js'
+//Khởi tạo 1 đối tượng trelloDatabaseInstace ban đầu là null (vì chưa connect)
+let trelloDatabaseInstance = null
+
+//Khởi tạo 1 đối tượng mongoClientInstance để connect tới mongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+//Kết nối tới database
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+//Function GET_DB  này có nhiệm vụ export ra cái Trello Database Instance
+//sau khi đã connect thành công tới MongoDB để chúng ta có thể sử dụng ở  nhiều nơi khác nhau trong code
+
+//chỉ  luôn gọi getDB khi đã connect DB thành công
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first')
+  return trelloDatabaseInstance
+}
+
+//Đóng kết nối tới database khi cần
+export const CLOSE_DB = async () => {
+  console.log('code chạy vào close')
+  await mongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,27 +1,32 @@
-.
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from './config/mongodb.js'
+import { env } from './config/environment.js'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen( () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Tu Cong Danh, I am running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+  // Thực hiện các tác vụ cleanup trước khi dừng server
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+CONNECT_DB()
+  .then(() => console.log('Conected'))
+  .then(() => START_SERVER())
+  .catch((error) => {
+    console.error(error)
+    process.exit(0)
+  })
