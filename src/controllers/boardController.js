88dacc85bbefcb99b816ee8c99486787ca42fb0d
TuCongDanh12 <tucondanh9a1@gmail.com>
2025-02-09 10:03:47 +0700

    service layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,12 +1,22 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from './../services/boardService.js'
 const createNew = async (req, res, next) => {
   try {
     // console.log('req.body: ', req.body)
     // console.log('req.query: ', req.query)
     // console.log('req.params: ', req.params)
+
+    //Điều hướng dữ liệu sang tầng service
+    const createBoard = await boardService.createNew(req.body)
+
+    //Có kết quả thì trả về phí client
+    // res
+    //   .status(StatusCodes.CREATED)
+    //   .json({ message: 'POST from Controller: API create new board' })
+
     res
       .status(StatusCodes.CREATED)
-      .json({ message: 'POST from Controller: API create new board' })
+      .json(createBoard)
   } catch (error) {
     next(error)
   }
